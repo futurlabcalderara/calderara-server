cd1805788b1dafa04c23596277b28a48220bc969
- MV - <79103102+MV-Studio14@users.noreply.github.com>
2022-09-05 21:59:43 +0200

    setup

---
+++ current/start.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.8.0_341\bin\java.exe" -Xmx10240M -Xms6144M -jar server_spigot.jar
---
+++ new/start.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jre1.8.0_341\bin\java.exe" -Xmx2048M -Xms512M -jar server_mohist.jar
